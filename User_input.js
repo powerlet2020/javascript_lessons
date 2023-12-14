66069be82692b5a127a450c504cda996192927af
Hamza Sulemana <powerlet2020@gmail.com>
2023-12-14 16:25:59 +0000

    addeded user input

---
+++ User_input.js
@@ -0,0 +1,11 @@
+const myName =prompt('Enter your name:');
+console.log("Hello",myName)
+
+const age = prompt('Enter your age:')
+console.log('your age is', age)
+
+console.log(typeof(age))
+
+
+const height = parseFloat(prompt("Enter your haeight"));
+console.log('your height is',height)
