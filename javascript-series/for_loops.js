741249e96ad800cdeb9651329dab525732d1e5b4
powerlet2020 <powerlet2020@gmail.com>
2024-05-22 08:06:13 +1200

    commited

--- javascript-series/for_loops.js
+++ javascript-series/for_loops.js
@@ -7,3 +7,18 @@ const fruits = ['orange','apple','mango','banana'];
 for(let fruit of fruits){
 console.log(fruit)
 }
+
+const student = {
+    name: 'hamza',
+    age: 30,
+    grade: 80
+}
+
+for (let data in  student){
+    console.log(`${data} => ${student[data]}`)
+}
+
+const language = 'python';
+ for (let character of language){
+    console.log(character)
+ }
